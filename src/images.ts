2b3d1ba7b228dd0cbc4f030415d0a312a0b12a09
Parthiv Kativarapu <parthiv.kativarapu@ideas.com>
2024-01-18 18:19:07 +0530

    Added all the pics; for now

---
+++ src/images.ts
@@ -0,0 +1,20 @@
+const pics: string[] = [
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/1.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/2.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/3.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/4.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/5.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/6.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/7.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/8.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/9.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/10.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/11.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/12.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/13.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/14.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/15.gif',
+  'https://github.com/parthiv13/treat-for-your-pr/blob/master/assets/16.gif',
+]
+
+export default pics
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+import pics from './images'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run(): Promise<void> {
+async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const randomCatGif = pics[Math.floor(Math.random() * pics.length)]
+    const message = `![Cat Gif](${randomCatGif})`
+    const githubToken = core.getInput('GITHUB_TOKEN')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const context = github.context
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.')
+      return
+    }
+    const pullRequestNumber = context.payload.pull_request.number
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = new github.GitHub(githubToken)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    octokit.issues.createComment({
+      ...context.repo,
+      issue_number: pullRequestNumber,
+      body: message
+    })
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(error.message)
   }
 }
+
+run()
